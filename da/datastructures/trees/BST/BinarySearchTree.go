23b0d4e41500fe979e128cce6816006ade05d180
DennisMao <raymond_2008@yahoo.com>
2019-01-11 18:06:20 +0800

    add BST

--- da/datastructures/trees/BST/BinarySearchTree.go
+++ da/datastructures/trees/BST/BinarySearchTree.go
@@ -1,10 +1,9 @@
-
-https://github.com/Workiva/go-datastructures/blob/master/tree/avl/avl.go
+//https://github.com/Workiva/go-datastructures/blob/master/tree/avl/avl.go
 // Name: Binary search tree
 package BinarySearchTree
 
 import (
-	"errors"
+	"log"
 )
 
 const (
@@ -26,21 +25,174 @@ type Node struct {
 	left   *Node
 	right  *Node
 	parent *Node
-	data   string
+
+	_high int // high
+
+	data string
 }
 
 type BinarySearchTree struct {
 	Root *Node
 }
 
-func (this *BinarySearchTree) Search(it string) {
+func New(it string) *BinarySearchTree {
+	root := new(Node)
+	root._high = 0
+	root.data = it
+
+	return &BinarySearchTree{root}
+}
+
+func (this *BinarySearchTree) Search(it string) *Node {
+
+	_hot := searchIn(it, this.Root)
+	if _hot.data != it {
+		return nil
+	} else {
+		return _hot
+	}
+}
+
+func searchIn(it string, _hot *Node) *Node {
+	if _hot == nil {
+		return nil
+	}
+
+	if _hot.data == it {
+		return _hot
+	}
+
+	if it < _hot.data {
+		if _hot.left != nil {
+			return searchIn(it, _hot.left)
+		}
+	} else {
+		if _hot.right != nil {
+			return searchIn(it, _hot.right)
+		}
+	}
+
+	return _hot
+}
+
+func updateAboveHigh(_hot *Node) {
 
 }
 
-func (this *BinarySearchTree) Insert(it string) {
+func (this *BinarySearchTree) Insert(it string) *Node {
+	_hot := searchIn(it, this.Root)
+	if _hot.data == it {
+		return _hot
+	}
+
+	newNode := new(Node)
+	newNode.parent = _hot
+	newNode.data = it
+	newNode._high = _hot._high + 1
+
+	if _hot.left == nil {
+		_hot.left = newNode
+	} else {
+		_hot.right = newNode
+	}
+
+	return newNode
 
 }
 
 func (this *BinarySearchTree) Remove(it string) {
 
+}
+
+// 镜像翻转
+func (tree *BinarySearchTree) Mirror(subNode *Node) {
+	if subNode == nil {
+		return
+	}
+
+	subNode.left, subNode.right = subNode.right, subNode.left
+	tree.Mirror(subNode.left)
+	tree.Mirror(subNode.right)
+}
+
+// 前序
+//（1）访问根结点
+//（2）前序遍历左子树
+//（3）前序遍历右子树
+func (tree *BinarySearchTree) PreorderTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	it(subNode.data)
+	tree.PreorderTraversal(subNode.left, it)
+	tree.PreorderTraversal(subNode.right, it)
+
+}
+
+// 中序
+//（1）中序遍历左子树
+//（2）访问根结点
+//（3）中序遍历右子树
+func (tree *BinarySearchTree) InorderTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	tree.InorderTraversal(subNode.left, it)
+	it(subNode.data)
+	tree.InorderTraversal(subNode.right, it)
+}
+
+// 后序
+//（1）后序遍历左子树
+//（2）后序遍历右子树
+//（3）访问根结点
+func (tree *BinarySearchTree) PostorderTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	if subNode.left != nil {
+		tree.PostorderTraversal(subNode.left, it)
+	}
+	if subNode.right != nil {
+		tree.PostorderTraversal(subNode.right, it)
+	}
+	it(subNode.data)
+
+}
+
+// 层级遍历
+// (1)从根部开始入栈
+// (2)出栈,输出
+// (3)左孩子入栈、右孩子入栈
+func (tree *BinarySearchTree) LevelTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	stack := make(chan *Node, 128)
+	stack <- subNode
+	for {
+		if len(stack) == 0 {
+			break
+		}
+
+		curNode := <-stack
+		if curNode == nil {
+			break
+		}
+
+		it(curNode.data) //输出
+
+		if curNode.left != nil {
+			stack <- curNode.left
+		}
+		if curNode.right != nil {
+			stack <- curNode.right
+		}
+	}
+
+	close(stack)
 }
---
+++ da/datastructures/trees/BST/BinarySearchTree_test.go
@@ -0,0 +1,131 @@
+package BinarySearchTree
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/bradleyjkemp/memviz"
+	"github.com/stretchr/testify/assert"
+)
+
+// 生成gv图
+func TestBinarySearchTree(t *testing.T) {
+
+	tree := New("D")
+
+	st := []string{"D", "B", "A", "C", "E", "F", "G"}
+	for i := 0; i < 6; i++ {
+		tree.Insert(st[i])
+	}
+
+	buf := &bytes.Buffer{}
+	memviz.Map(buf, tree)
+
+	ioutil.WriteFile("./BinarySearchTree.gv", buf.Bytes(), os.ModePerm)
+	exec.Command("dot", "-Tpng", "./BinarySearchTree.gv", "-o", "./BinarySearchTree.png").Run()
+}
+
+// 搜索
+func TestSearch(t *testing.T) {
+	tree := New("D")
+
+	//st := []string{"D", "B", "F", "A", "C", "E"}
+	st := []string{"D", "B", "A", "C", "E", "F", "G"}
+	for i := 0; i < 6; i++ {
+		tree.Insert(st[i])
+	}
+
+	node := tree.Search("D")
+
+	assert.Equal(t, "D", node.data)
+}
+
+// 前序
+// Root [Left] [Right]
+func TestPreorderTraversal(t *testing.T) {
+	tree := New("D")
+
+	st := []string{"D", "B", "F", "A", "C", "E"}
+	for i := 0; i < 6; i++ {
+		tree.Insert(st[i])
+	}
+
+	result := ""
+	it := func(i string) bool {
+
+		fmt.Printf(" %s", i)
+		result += i
+		return true
+	}
+
+	tree.PreorderTraversal(tree.Root, it)
+	assert.Equal(t, "DBACFE", result)
+
+}
+
+// 中序
+// [Left] Root [Right]
+func TestInorderTraversal(t *testing.T) {
+	tree := New("D")
+
+	st := []string{"D", "B", "F", "A", "C", "E"}
+	for i := 0; i < 6; i++ {
+		tree.Insert(st[i])
+	}
+
+	result := ""
+	it := func(i string) bool {
+		fmt.Printf(" %s", i)
+		result += i
+		return true
+	}
+
+	tree.InorderTraversal(tree.Root, it)
+	assert.Equal(t, "ABCDEF", result)
+}
+
+// 后序
+// [Right] [Left] Root
+func TestPostorderTraversal(t *testing.T) {
+	tree := New("D")
+
+	st := []string{"D", "B", "F", "A", "C", "E"}
+	for i := 0; i < 6; i++ {
+		tree.Insert(st[i])
+	}
+
+	result := ""
+	it := func(i string) bool {
+		fmt.Printf(" %s", i)
+		result += i
+		return false
+	}
+
+	tree.PostorderTraversal(tree.Root, it)
+	assert.Equal(t, "ACBEFD", result)
+}
+
+// 层级遍历
+// ROOT [h2] ... [hn]
+func TestLevelTraversal(t *testing.T) {
+	tree := New("D")
+
+	st := []string{"D", "B", "F", "A", "C", "E"}
+	for i := 0; i < 6; i++ {
+		tree.Insert(st[i])
+	}
+
+	result := ""
+	it := func(i string) bool {
+		fmt.Printf(" %s", i)
+		result += i
+		return false
+	}
+
+	tree.LevelTraversal(tree.Root, it)
+	assert.Equal(t, "DBFACE", result)
+}
