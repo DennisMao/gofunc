fbdb37297a5660a589f4cb728654634ae83c9486
DennisMao <raymond_2008@yahoo.com>
2019-01-29 19:05:50 +0800

    add rotate

--- da/datastructures/trees/AVL/AvlTree.go
+++ da/datastructures/trees/AVL/AvlTree.go
@@ -1,8 +1,8 @@
-// Name: Binary tree
+// Name: Binary search tree
 package AvlTree
 
 import (
-	BST "goexperience/da/datastructures/trees/BST"
+	"fmt"
 )
 
 const (
@@ -11,18 +11,124 @@ const (
 	CompareMore
 )
 
-type Node=BST.Node
+// ItemIterator allows callers of Ascend* to iterate in-order over portions of
+// the tree.  When this function returns false, iteration will stop and the
+// associated Ascend* function will immediately return.
+type ItemIterator func(i string) bool
 
-type AvlTree struct {
-	BST.BinarySearchTree
+// stringCompare allows callers specify the compare function for items searching.
+// Return the compare state
+type ItemCompare func(i string) int
+
+type Node struct {
+	left, right, parent *Node
+	height              int
+	data                string
 }
 
-// 插入
-// @Override
-func (tree *AvlTree) Insert(item string) {
+type AvlTree struct {
+	Root *Node
+}
+
+////////////////////// Node operation //////////////////////
+func (this *Node) Data() string {
+	return this.data
+}
+
+func Height(sub *Node) int {
+	if sub == nil {
+		return 0
+	}
+
+	return sub.height
+}
+
+func LChild(sub *Node) *Node {
+	if sub == nil {
+		return nil
+	}
+	return sub.left
+}
+
+func RChild(sub *Node) *Node {
+	if sub == nil {
+		return nil
+	}
+	return sub.right
+}
+
+func Parent(sub *Node) *Node {
+	if sub == nil {
+		return nil
+	}
+	return sub.parent
+}
+
+// CollectHeight will collect the height for a node itself.
+func CollectHeight(sub *Node) {
+	sub.height = getHeight(sub)
+}
+
+// CollectHeightDeep will collect the height property for a node including its childrens.
+func CollectHeightDeep(sub *Node) {
+	sub.height = getHeight(sub)
+}
+
+func IncreaseHeight(sub *Node) {
+	sub.height++
+}
+
+func getHeight(sub *Node) int {
+	if sub == nil {
+		return 0
+	}
+
+	leftHeight := getHeight(sub.left)
+	rightHeight := getHeight(sub.right)
+
+	// Get Max(leftHeight,rightHeight)
+	if leftHeight < rightHeight {
+		return rightHeight + 1
+	} else {
+		return leftHeight + 1
+	}
+}
+
+func isBalanced(sub *Node) bool {
+	balF := balFactor(sub)
+	return balF > -2 && balF < 2
+}
+
+func balFactor(sub *Node) int {
+	return getHeight(sub.left) - getHeight(sub.right)
+}
+
+// reBalanced can reBalanced an unbalanced node by 3-4 reconsitution way.
+// Firstly,it will find the unbalanced type of the node (including zigzig,zagzag,zigzag,zagzig).
+// And it calls 'rotate34At' with corresponding paramters.
+func reBalanced(g *Node) {
+	/*
+		1.judege whether 'g' is balanced in recursion
+		2.judege types zigzig or zigzag  respectively
+		3.call ratate34At to rebalanced the node
+	*/
+}
+
+// rotate34At can adjust nodes and rebalance by 3-4 reconsitution.
+func rotate34At(a, b, c, t1, t2, t3, t4 *Node) {
+
+}
+
+////////////////////// Tree operation //////////////////////
+func New() *AvlTree {
+
+	return &AvlTree{nil}
+
+}
+
+func (tree *AvlTree) Insert(it string) *Node {
 	if tree.Root == nil {
-		tree.Root = &Node{}
-	
+		tree.Root = &Node{nil, nil, nil, 0, it}
 	}
 
 	insertPos := searchWithHot(tree.Root, it) // find a suitable insert postition
@@ -45,12 +151,18 @@ func (tree *AvlTree) Insert(item string) {
 		insertPos.right = &newNode
 	}
 
+	return &newNode
+
+	// Rebalanced
+
 }
 
-// 删除
-// @Override
-func (tree *AvlTree) Remove(item string) {
+func (tree *AvlTree) Search(it string) *Node {
+	if tree.Root == nil {
+		return nil
+	}
 
+	return search(tree.Root, it)
 }
 
 // searchWithHot can find the data 'it' in a tree using recursive algorithm,
@@ -83,20 +195,170 @@ func searchWithHot(sub *Node, it string) *Node {
 
 	return nil
 }
-////////////////////// 平衡 ///////////////////
-func BalancedFactor(subNode *Node) int {
-	return Height(LChild(subNode)) - Height(RChild(subNode))
+
+// search can find the data 'it' in a tree using recursive algorithm,
+// and return a pointer pointed to 'it' if find while return 'nil' if not find.
+func search(sub *Node, it string) *Node {
+	if sub == nil {
+		return nil
+	}
+
+	if sub.data == it {
+		return sub
+	}
+
+	if sub.data > it {
+		return search(sub.left, it)
+	} else {
+		return search(sub.right, it)
+	}
+
+	return nil
 }
 
-///////////////////// 辅助函数 ////////////////
-func LChild(sub *Node) *Node {
-	return BST.LChild(sub)
+// Delete can delete a specifed node with data 'it'
+func (tree *AvlTree) Delete(it string) error {
+	pos := search(tree.Root, it)
+	if pos == nil {
+		return fmt.Errorf("Can not find the node with data '%s'", it)
+	}
+
+	if pos.data != it {
+		return fmt.Errorf("Can not find the node with data '%s'", it)
+	}
+
+	return delete(pos)
 }
 
-func RChild(sub *Node) *Node {
-	return BST.RChild(sub)
+func delete(sub *Node) error {
+	/*分三种情况,需要删除的点为
+	1.叶子结点,将父亲关联去掉释放结点
+	2.仅有左子树或者右子树
+		1.右子树为空,从新连接为左子树
+		2.左子树为空,从新连接为右子树
+	3.同时有左子树或者右子树
+		1.将待删除点替换成'左孩子'的'最右子树结点'结点(左子树中的最大值)
+	*/
+	if sub == nil {
+		return fmt.Errorf("Can not find the node")
+	}
+
+	if sub.left != nil && sub.right != nil {
+		// 左右都有子树
+		maxLeftChild := sub.left //转向左子树,找到左子树中的最大值
+
+		for maxLeftChild.right != nil {
+			maxLeftChild = maxLeftChild.right
+		}
+
+		sub.data = maxLeftChild.data //待删节点换成左子树最大值结点
+		return delete(maxLeftChild)
+
+	} else if sub.left != nil {
+		// 拥有左子树
+		sub.data = sub.left.data
+		sub.left = sub.left.left
+		sub.right = sub.left.right
+		return nil
+
+	} else if sub.right != nil {
+		// 拥有右子树
+		sub.data = sub.right.data
+		sub.left = sub.right.left
+		sub.right = sub.right.right
+		return nil
+
+	} else {
+		// 叶子节点
+		if sub.parent.left == sub {
+			sub.parent.left = nil
+		} else {
+			sub.parent.right = nil
+		}
+
+	}
+
+	return fmt.Errorf("Delete failed")
 }
 
-func Height(sub *Node) int {
-	return BST.Height(sub)
+// 前序
+//（1）访问根结点
+//（2）前序遍历左子树
+//（3）前序遍历右子树
+func (tree *AvlTree) PreorderTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	it(subNode.data)
+	tree.PreorderTraversal(subNode.left, it)
+	tree.PreorderTraversal(subNode.right, it)
+
+}
+
+// 中序
+//（1）中序遍历左子树
+//（2）访问根结点
+//（3）中序遍历右子树
+func (tree *AvlTree) InorderTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	tree.InorderTraversal(subNode.left, it)
+	it(subNode.data)
+	tree.InorderTraversal(subNode.right, it)
+}
+
+// 后序
+//（1）后序遍历左子树
+//（2）后序遍历右子树
+//（3）访问根结点
+func (tree *AvlTree) PostorderTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	if subNode.left != nil {
+		tree.PostorderTraversal(subNode.left, it)
+	}
+	if subNode.right != nil {
+		tree.PostorderTraversal(subNode.right, it)
+	}
+	it(subNode.data)
+
+}
+
+// 层级遍历
+// (1)从根部开始入栈
+// (2)出栈,输出
+// (3)左孩子入栈、右孩子入栈
+func (tree *AvlTree) LevelTraversal(subNode *Node, it ItemIterator) {
+	if subNode == nil {
+		return
+	}
+
+	stack := make(chan *Node, 128)
+	stack <- subNode
+	for {
+		if len(stack) == 0 {
+			break
+		}
+
+		curNode := <-stack
+		if curNode == nil {
+			break
+		}
+
+		it(curNode.data) //输出
+
+		if curNode.left != nil {
+			stack <- curNode.left
+		}
+		if curNode.right != nil {
+			stack <- curNode.right
+		}
+	}
+
+	close(stack)
 }
